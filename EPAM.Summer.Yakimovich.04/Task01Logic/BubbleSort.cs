5b731f003756101bd3a2f1c772a0e57b81dd5f85
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-12 19:15:58 +0300

    added classes-comparators

--- EPAM.Summer.Yakimovich.04/Task01Logic/BubbleSort.cs
+++ EPAM.Summer.Yakimovich.04/Task01Logic/BubbleSort.cs
@@ -1,48 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Task01Logic
 {
     public static class BubbleSort
     {
-        /// <summary>
-        /// Bubble Sort algorithm with 6 different ways of sorting
-        /// </summary>
-        /// <param name="arr">array</param>
-        /// <param name="criterion">criterion of comparison 1 - sum ascending, 2 - sum descending, 
-        /// 3 - max values ascending, 4 - max values descending, 5- min values ascending, 6 - min values descending</param>
-        public static void BubbleSortMethod(int[][] arr, int criterion)
+        public static void BubbleSortMethod(int[][] arr, IComparer<int[]> comparation )
         {
             if (arr == null)
             {
-                throw new ArgumentException();
+                throw new NullReferenceException();
             }
 
-            switch (criterion)
+            if (comparation == null)
             {
-                case 1:
-                    BubbleSortSumAscend(arr);
-                    break;
-                case 2:
-                    BubbleSortSumDescend(arr);
-                    break;
-                case 3:
-                    BubbleSortMaxAscend(arr);
-                    break;
-                case 4:
-                    BubbleSortMaxDescend(arr);
-                    break;
-                case 5:
-                    BubbleSortMinAscend(arr);
-                    break;
-                case 6:
-                    BubbleSortMinDescend(arr);
-                    break;
-                default: throw new ArgumentException();
+                DefaultBubbleSort(arr);
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    for (int j = 0; j < arr.Length - i - 1; j++)
+                    {
+                        if (comparation.Compare(arr[j], arr[j + 1]) == 1)
+                        {
+                            Swap(ref arr[j], ref arr[j + 1]);
+                        }
+                    }
+                }
             }
         }
 
-        public static void BubbleSortSumAscend(int[][] arr)
+        private static void DefaultBubbleSort(int[][] arr)
         {
             for (int i = 0; i < arr.Length - 1; i++)
             {
@@ -50,92 +40,17 @@ namespace Task01Logic
                 {
                     if (arr[j].Sum() > arr[j + 1].Sum())
                     {
-                        int[] temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
+                        Swap(ref arr[j], ref arr[j + 1]);
                     }
                 }
             }
         }
 
-        public static void BubbleSortSumDescend(int[][] arr)
+        private static void Swap(ref int[] a, ref int[] b)
         {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (arr[j].Sum() < arr[j + 1].Sum())
-                    {
-                        int[] temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                }
-            }
-        }
-
-        public static void BubbleSortMaxAscend(int[][] arr)
-        {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (arr[j].Max() > arr[j + 1].Max())
-                    {
-                        int[] temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                }
-            }
-        }
-
-        public static void BubbleSortMaxDescend(int[][] arr)
-        {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (arr[j].Max() < arr[j + 1].Max())
-                    {
-                        int[] temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                }
-            }
-        }
-
-        public static void BubbleSortMinAscend(int[][] arr)
-        {
-            for (int i = 0; i < arr.Length ; i++)
-            {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (arr[j].Min() > arr[j + 1].Min())
-                    {
-                        int[] temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                }
-            }
-        }
-
-        public static void BubbleSortMinDescend(int[][] arr)
-        {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (arr[j].Min() < arr[j + 1].Min())
-                    {
-                        int[] temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                }
-            }
+            int[] temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
---
+++ EPAM.Summer.Yakimovich.04/Task01Tests/Comparators.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01Tests
+{
+    class SumAscending : IComparer<int[]>
+    {
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            return lhs.Sum() > rhs.Sum() ? 1 : lhs.Sum() == rhs.Sum() ? 0 : -1;
+        }
+    }
+
+    class SumDscending : IComparer<int[]>
+    {
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            return lhs.Sum() < rhs.Sum() ? 1 : lhs.Sum() == rhs.Sum() ? 0 : -1;
+        }
+    }
+
+    class MaxElementAscending : IComparer<int[]>
+    {
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            return lhs.Max() > rhs.Max() ? 1 : lhs.Max() == rhs.Max() ? 0 : -1;
+        }
+    }
+
+    class MaxElementDescending : IComparer<int[]>
+    {
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            return lhs.Max() < rhs.Max() ? 1 : lhs.Max() == rhs.Max() ? 0 : -1;
+        }
+    }
+
+    class MinElementAscending : IComparer<int[]>
+    {
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            return lhs.Min() > rhs.Min() ? 1 : lhs.Min() == rhs.Min() ? 0 : -1;
+        }
+    }
+
+    class MinElementDescending : IComparer<int[]>
+    {
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            return lhs.Min() < rhs.Min() ? 1 : lhs.Min() == rhs.Min() ? 0 : -1;
+        }
+    }
+}
